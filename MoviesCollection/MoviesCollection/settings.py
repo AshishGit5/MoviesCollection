bb9575f4dcbfdaa366cd938cb6869d4e796cf2ad
AshishGit5 <ranwalkar.ashish@gmail.com>
2023-04-28 09:06:41 +0530

    Added .env File

--- MoviesCollection/MoviesCollection/settings.py
+++ MoviesCollection/MoviesCollection/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uu*%!d1z4fy7s^e_uu)pb_g7jrozjd^y-y!noba4twuwz&e%cr"
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
